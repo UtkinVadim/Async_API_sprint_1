d78b49e29a1ec7c16d8ff769e1fc0d69288c74b6
UtkinVadim <utkinvadim1991@gmail.com>
2021-11-21 01:35:53 +0300

    Создана модель Person.

---
+++ src/models/person.py
@@ -0,0 +1,18 @@
+import orjson
+
+from typing import List
+from pydantic import BaseModel
+
+from models.film import Film
+from models.utils import orjson_dumps
+
+
+class Person(BaseModel):
+    uuid: str
+    full_name: str
+    role: List[str]
+    film_ids: List[Film]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
