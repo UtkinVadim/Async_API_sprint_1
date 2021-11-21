9f3fbcb9ef21345e32d6054b494404a89f158716
UtkinVadim <utkinvadim1991@gmail.com>
2021-11-21 15:54:22 +0300

    Перенесен импорт модели Film, чтобы избежать "circular import".

--- src/models/person.py
+++ src/models/person.py
@@ -3,7 +3,6 @@ import orjson
 from typing import List
 from pydantic import BaseModel
 
-from models.film import Film
 from models.utils import orjson_dumps
 
 
@@ -11,8 +10,13 @@ class Person(BaseModel):
     uuid: str
     full_name: str
     role: str
-    film_ids: List[Film]
+    film_ids: "List[Film]"
 
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
+
+
+from models.film import Film  # noqa: E402
+
+Person.update_forward_refs()
