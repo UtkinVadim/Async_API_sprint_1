18a7bf28a28063ba90510dd96e73e9b6c16a6b38
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-11-21 15:50:19 +0300

    Реализован поиск по фильмам с пагинацией, фильтрацией по жанрам и сортировкой. Добавлены функции генерации тела запроса

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
+from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
-
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
@@ -16,7 +16,17 @@ class Film(BaseModel):
 # Внедряем FilmService с помощью Depends(get_film_service)
 @router.get("/{film_id}", response_model=Film)
 async def film_details(
-    film_id: str, film_service: FilmService = Depends(get_film_service)
+    film_id: str,
+    from_: Optional[str] = Query(
+        None,
+        alias="page[number]",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+    ),
+    size: Optional[str] = Query(None, alias="page[size]"),
+    sort: Optional[str] = Query(None, regex="-?imdb_rating"),
+    filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
+    film_service: FilmService = Depends(get_film_service),
 ) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
@@ -32,3 +42,91 @@ async def film_details(
     # вы бы предоставляли клиентам данные, которые им не нужны
     # и, возможно, данные, которые опасно возвращать
     return Film(id=film.id, title=film.title)
+
+
+# GET /api/v1/film?filter[genre]=<uuid:UUID>&sort=-imdb_rating&page[size]=50&page[number]=1
+# GET /api/v1/film?sort=-imdb_rating&page[size]=50&page[number]=1
+# Полная информация по фильму.
+# /api/v1/film/<uuid:UUID>/
+@router.get("/search/")
+async def film_search(
+    query: Optional[str] = Query("", alias="query"),
+    from_: Optional[str] = Query(
+        None,
+        alias="page[number]",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+    ),
+    size: Optional[str] = Query(None, alias="page[size]"),
+    sort: Optional[str] = Query(None, regex="-?imdb_rating"),
+    filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
+    filter_person_id: Optional[str] = Query(None, alias="filter[person]"),
+    film_service: FilmService = Depends(get_film_service),
+) -> list[Film]:
+    """
+    Поиск по фильмам с пагинацией, фильтрацией по жанрам и сортировкой
+
+    :param query:
+    :param from_:
+    :param size:
+    :param sort:
+    :param filter_genre_id:
+    :param filter_person_id:
+    :param film_service:
+    :return:
+    """
+    body = await generate_body(query, from_, size)
+
+    if sort:
+        body = await add_sort_to_body(body, sort)
+
+    if filter_genre_id:
+        filter_genre = await film_service.get_by_id(filter_genre_id, index="genre")
+        body = await add_filter_to_body(body, filter_genre)
+
+    result = await film_service.search(index="movies", body=body)
+
+    if not result:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
+
+    return result
+
+
+async def generate_body(query, from_, size) -> dict:
+    if not query:
+        query = ""
+
+    body = {"query": {"bool": {"must": [{"multi_match": {"query": query}}]}}}
+
+    if from_:
+        body["from"] = from_
+    if size:
+        body["from"] = size
+
+    return body
+
+
+async def add_sort_to_body(body, sort) -> dict:
+    if "-" in sort:
+        sort = [{"imdb_rating": "desc"}]
+    else:
+        sort = [{"imdb_rating": "asc"}]
+    body["sort"] = sort
+    return body
+
+
+async def add_filter_to_body(body, filter_genre) -> dict:
+    filter_dict = {
+        "bool": {
+            "filter": {
+                "bool": {
+                    "must": {  # если делать поиск и по жанрам и по актёрам сюда нужно поставить should
+                        "term": {"genre": filter_genre.name}
+                    }
+                }
+            }
+        }
+    }
+
+    body["query"]["bool"]["must"].append(filter_dict)
+    return body
