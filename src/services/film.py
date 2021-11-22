f4020c7c98006ca6826c87ff038e1e649131d528
Alexandr Galanov <alexandr.galanov@gmail.com>
2021-11-22 12:44:12 +0300

    Разделен search на непостредвенно поиск данных в эластике и обрёткой которая сначала ищет данные в кеше. Добавлена функеция преобразования словаря в md5. Реализована функция взятия кеша для запросов которые делаются с помощью body. Функция _put_obj_to_cache переделана на универсальную работу с любыми объектами

--- src/services/film.py
+++ src/services/film.py
@@ -1,6 +1,8 @@
+import hashlib
 import logging
 from functools import lru_cache
 from typing import List, Optional, Union
+import json
 
 from aioredis import Redis
 from db.elastic import get_elastic
@@ -46,12 +48,23 @@ class FilmService:
             await self._put_obj_to_cache(obj)
         return obj
 
-    async def search(self, index: str, body: dict) -> List:
-        doc = await self.elastic.search(index=index, body=body)
-        doc = doc.get("hits", {})
-        doc = doc.get("hits", [])
-        doc = [Film(**d["_source"]) for d in doc]
-        return doc
+    async def search(self, index: str, body: dict) -> Optional[List]:
+        docs = await self._get_from_cache_by_body(body, index)
+        if not docs:
+            docs = await self._search_in_elastic(index=index, body=body)
+            if not docs:
+                return None
+            key = await self._str2md5(body)
+            await self._put_obj_to_cache(docs, key=key)
+
+        return docs
+
+    async def _search_in_elastic(self, body: dict, index: str) -> Optional[Film]:
+        docs = await self.elastic.search(index=index, body=body)
+        docs = docs.get("hits", {})
+        docs = docs.get("hits", [])
+        docs = [Film(**d["_source"]) for d in docs]
+        return docs
 
     async def _get_by_id_from_elastic(self, id_: str, index: str) -> Optional[Film]:
         try:
@@ -80,7 +93,38 @@ class FilmService:
         obj = data_model.parse_raw(data)
         return obj
 
-    async def _put_obj_to_cache(self, obj: Union[Film]):
+    async def _str2md5(self, body: dict) -> str:
+        """
+        Возвращает md5 от словаря с запросом к эластику.
+
+        :param body:
+        :return:
+        """
+
+        return hashlib.md5(str(body).encode("utf-8")).hexdigest()
+
+    async def _get_from_cache_by_body(
+        self, body: dict, index: str
+    ) -> Optional[list[Film]]:
+        """
+        Забирает данные из кеша по ключу (body).
+        Полученные данные вставляет в модель данных
+
+        :param body:
+        :param index:
+        :return:
+        """
+        key = await self._str2md5(body)
+        data = await self.redis.get(key)
+        if not data:
+            return None
+
+        data_model = self.models[index]
+        data = json.loads(data)["result"]
+        obj = [data_model.parse_raw(d) for d in data]
+        return obj
+
+    async def _put_obj_to_cache(self, obj: Union[Film, Person, Genre], key: str = None):
         """
         Сохраняем данные о фильме, используя команду set
         Выставляем время жизни кеша — 5 минут
@@ -90,7 +134,15 @@ class FilmService:
         :param obj:
         :return:
         """
-        await self.redis.set(obj.id, obj.json(), expire=FILM_CACHE_EXPIRE_IN_SECONDS)
+
+        if not key:
+            key = obj.id
+            data_to_cache = obj.json()
+
+        else:
+            data_to_cache = json.dumps({"result": [d.json() for d in obj]})
+
+        await self.redis.set(key, data_to_cache, expire=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
 @lru_cache()
