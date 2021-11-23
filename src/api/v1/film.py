f09e1f0bf9e25fe7bfc3a06e3cc2e707993a71bf
UtkinVadim <utkinvadim1991@gmail.com>
2021-11-24 01:46:35 +0300

    Убраны слэши в апи =) Апи расставлены в правильном порядке.

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -37,53 +37,7 @@ class FilmDetailed(BaseModel):
     writers: Optional[list[FilmPerson]]
 
 
-@router.get("/{film_id}", response_model=FilmDetailed)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmDetailed:
-    """
-    Отдаёт полную информацию по фильму
-    GET /api/v1/film/<uuid:UUID>/
-
-    :param film_id:
-    :param film_service:
-    :return:
-    """
-    film = await film_service.get_by_id(id_=film_id)
-    if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
-
-    return FilmDetailed(**film.dict())
-
-
-@router.get("/", response_model=List[ShortFilm])
-async def film_filter(
-    from_: Optional[str] = Query(
-        None,
-        alias="page[number]",
-        title="страница",
-        description="Порядковый номер страницы результатов",
-    ),
-    size: Optional[str] = Query(
-        None,
-        alias="page[size]",
-        title="размер страницы",
-        description="Количество документов на странице",
-    ),
-    sort: Optional[str] = Query("imdb_rating", regex="-?imdb_rating"),
-    filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
-    film_service: FilmService = Depends(get_film_service),
-) -> List[ShortFilm]:
-    result = await film_search(
-        query=None,
-        from_=from_,
-        size=size,
-        sort=sort,
-        filter_genre_id=filter_genre_id,
-        film_service=film_service,
-    )
-    return result
-
-
-@router.get("/search/", response_model=List[ShortFilm])
+@router.get("/search", response_model=List[ShortFilm])
 async def film_search(
     query: Optional[str] = Query("", alias="query"),
     from_: Optional[str] = Query(
@@ -132,4 +86,50 @@ async def film_search(
     if not result:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
 
+    return result
+
+
+@router.get("/{film_id}", response_model=FilmDetailed)
+async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmDetailed:
+    """
+    Отдаёт полную информацию по фильму
+    GET /api/v1/film/<uuid:UUID>/
+
+    :param film_id:
+    :param film_service:
+    :return:
+    """
+    film = await film_service.get_by_id(id_=film_id)
+    if not film:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+
+    return FilmDetailed(**film.dict())
+
+
+@router.get("/", response_model=List[ShortFilm])
+async def film_filter(
+    from_: Optional[str] = Query(
+        None,
+        alias="page[number]",
+        title="страница",
+        description="Порядковый номер страницы результатов",
+    ),
+    size: Optional[str] = Query(
+        None,
+        alias="page[size]",
+        title="размер страницы",
+        description="Количество документов на странице",
+    ),
+    sort: Optional[str] = Query("imdb_rating", regex="-?imdb_rating"),
+    filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
+    film_service: FilmService = Depends(get_film_service),
+) -> List[ShortFilm]:
+    result = await film_search(
+        query=None,
+        from_=from_,
+        size=size,
+        sort=sort,
+        filter_genre_id=filter_genre_id,
+        film_service=film_service,
+    )
     return result
