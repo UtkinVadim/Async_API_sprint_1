13c24087a8205aa46bf85f0bd5ff5f4168eca6f9
UtkinVadim <utkinvadim1991@gmail.com>
2021-11-24 01:06:04 +0300

    Удалены лишние комментарии, код приведен к единому стилю.

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import APIRouter, Depends, HTTPException, Query
-from models.film import Film
+
 from pydantic import BaseModel
+
+from api.utils import add_filter_to_body, add_sort_to_body, generate_body
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
@@ -35,11 +37,8 @@ class FilmDetailed(BaseModel):
     writers: Optional[list[FilmPerson]]
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
 @router.get("/{film_id}", response_model=FilmDetailed)
-async def film_details(
-    film_id: str, film_service: FilmService = Depends(get_film_service)
-) -> Film:
+async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmDetailed:
     """
     Отдаёт полную информацию по фильму
     GET /api/v1/film/<uuid:UUID>/
@@ -48,15 +47,14 @@ async def film_details(
     :param film_service:
     :return:
     """
-
-    film = await film_service.get_by_id(id_=film_id, index="movies")
+    film = await film_service.get_by_id(id_=film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
 
     return FilmDetailed(**film.dict())
 
 
-@router.get("/", response_model=list[ShortFilm])
+@router.get("/", response_model=List[ShortFilm])
 async def film_filter(
     from_: Optional[str] = Query(
         None,
@@ -73,7 +71,7 @@ async def film_filter(
     sort: Optional[str] = Query("imdb_rating", regex="-?imdb_rating"),
     filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
     film_service: FilmService = Depends(get_film_service),
-) -> list[ShortFilm]:
+) -> List[ShortFilm]:
     result = await film_search(
         query=None,
         from_=from_,
@@ -85,7 +83,7 @@ async def film_filter(
     return result
 
 
-@router.get("/search/", response_model=list[ShortFilm])
+@router.get("/search", response_model=List[ShortFilm])
 async def film_search(
     query: Optional[str] = Query("", alias="query"),
     from_: Optional[str] = Query(
@@ -103,7 +101,7 @@ async def film_search(
     sort: Optional[str] = Query(None, regex="-?imdb_rating"),
     filter_genre_id: Optional[str] = Query(None, alias="filter[genre]"),
     film_service: FilmService = Depends(get_film_service),
-) -> list[ShortFilm]:
+) -> Optional[List[ShortFilm]]:
     """
     Поиск по фильмам с пагинацией, фильтрацией по жанрам и сортировкой
 
@@ -112,7 +110,6 @@ async def film_search(
     :param size:
     :param sort:
     :param filter_genre_id:
-    :param filter_person_id:
     :param film_service:
     :return:
     """
@@ -127,80 +124,12 @@ async def film_search(
     if filter_genre_id:
         filter_genre = await film_service.get_by_id(filter_genre_id, index="genre")
         if not filter_genre:
-            raise HTTPException(
-                status_code=HTTPStatus.NOT_FOUND, detail="films not found"
-            )
+            raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
         body = await add_filter_to_body(body, filter_genre)
 
-    result = await film_service.search(index="movies", body=body)
+    result = await film_service.search(body=body)
 
     if not result:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
 
     return result
-
-
-async def generate_body(query, from_, size) -> dict:
-    """
-    Создаёт тело запроса к эластику.
-    Если query не задан - возвращает все документы.
-
-    :param query:
-    :param from_: номер выводимой страницы
-    :param size: кол-во данных (документов) на странице
-    :return:
-    """
-
-    if not query:
-        match = {"match_all": {}}
-    else:
-        match = {"multi_match": {"query": query}}
-
-    body = {"query": {"bool": {"must": [match]}}}
-
-    if from_:
-        body["from"] = from_
-    if size:
-        body["size"] = size
-
-    return body
-
-
-async def add_sort_to_body(body, sort) -> dict:
-    """
-    Добавляет в тело зарпоса сортировку
-
-    :param body:
-    :param sort:
-    :return:
-    """
-    if "-" in sort:
-        sort = [{"imdb_rating": "desc"}]
-    else:
-        sort = [{"imdb_rating": "asc"}]
-    body["sort"] = sort
-    return body
-
-
-async def add_filter_to_body(body, filter_genre) -> dict:
-    """
-    Добавляет в тело запроса условие фильтрации по жанру
-
-    :param body:
-    :param filter_genre:
-    :return:
-    """
-    filter_dict = {
-        "bool": {
-            "filter": {
-                "bool": {
-                    "must": {  # если делать поиск и по жанрам и по актёрам сюда нужно поставить should
-                        "term": {"genre": filter_genre.name}
-                    }
-                }
-            }
-        }
-    }
-
-    body["query"]["bool"]["must"].append(filter_dict)
-    return body
